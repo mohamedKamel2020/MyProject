2804421a2c9e888dc0de3786f003d040ae0e67c4
mohamedkamel2020 <m.kamelmohamed2020@gmail.com>
2023-03-05 15:33:56 +0200

    add project

---
+++ myProject/BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class BethanysPieShopDbContext: DbContext
+    {
+
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options){}
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP-JDMM5F1\\MSSQLSERVER01; Initial Catalog = BethanysPieShop8854;Integrated Security = SSPI ; TrustServerCertificate =True ");
+        }
+    }
+}
